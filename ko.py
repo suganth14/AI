92a105d023559520e3abe0c41f7d52b8f5d6293a
suganth14 <69384507+suganth14@users.noreply.github.com>
2022-05-12 07:51:31 +0530

    Update ko.py

--- ko.py
+++ ko.py
@@ -1,25 +1,21 @@
-import math
-ipV = [[0.2,0.4]]
-# ipV = [[-1,0]]
+def updateW(index,inpIndex):
+    for i in range(len(W)):
+        W[i][index] = W[i][index] + alpha*(I[inpIndex][i]-W[i][index])
 
-w = [[0.5,0.2],[0.1,0.4],[0.1,0.4],[0.6,0.9],[0.9,0.1]]
-# w = [[0,-1],[-2/math.sqrt(5),1/math.sqrt(5)],[-1/math.sqrt(5),2/math.sqrt(5)]]
+W = [
+    [0.5,0.1,0.1,0.6,0.9],
+    [0.2,0.4,0.4,0.9,0.1]
+    ]
+
+I = [[0.2,0.4]]
 alpha = 0.2
 
-
-for i in range(len(ipV)):
-    c= []
-    for j in range(len(w)):
-        x = 0
-        for k in range(len(ipV[i])):
-            x += (ipV[i][k] - w[j][k])**2
-        c.append(x)
-    
-    pos = c.index(min(c))
-
-    
-    for j in range(len(ipV[i])):
-        w[pos][j] = w[pos][j] + alpha*(ipV[i][j] - w[pos][j])
-        
-    
-print(w)
+for i in range(len(I)):
+    arr = []
+    for j in range(len(W[0])):
+        val = 0
+        for k in range(len(W)):
+            val+=I[i][k]*W[k][j]
+        arr.append(val)
+    updateW(arr.index(min(arr)),i)
+print(W)
